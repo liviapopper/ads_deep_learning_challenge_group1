2646b0551d764e3b8e5a14d1ee0af2ffa119d410
bastiaanv <bastiankpn7800@gmail.com>
2020-11-26 12:50:15 +0100

    fixed formatting of model

--- model.py
+++ model.py
@@ -5,13 +5,27 @@ from tensorflow.keras.layers import Input, Dense, Conv2D, Flatten, BatchNormaliz
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras import regularizers
 
-from loss import softmax_cross_entropy_with_logits
+import tensorflow as tf
+
+def softmax_cross_entropy_with_logits(y_true, y_pred):
+	p = y_pred
+	pi = y_true
+
+	zero = tf.zeros(shape = tf.shape(pi), dtype=tf.float32)
+	where = tf.equal(pi, zero)
+
+	negatives = tf.fill(tf.shape(pi), -100.0) 
+	p = tf.where(where, negatives, p)
+
+	return tf.nn.softmax_cross_entropy_with_logits(labels = pi, logits = p)
+
 
 class Residual_CNN():
 	def __init__(self, reg_const, learning_rate, input_dim,  output_dim, hidden_layers):
 		self.hidden_layers = hidden_layers
 		self.num_layers = len(hidden_layers)
-		self.model = self._build_model(input_dim)
+		self.reg_const = reg_const
+		self.model = self._build_model(input_dim, output_dim, learning_rate)
 
 	def predict(self, x):
 		return self.model.predict(x)
@@ -20,12 +34,12 @@ class Residual_CNN():
 		return self.model.fit(states, targets, epochs=epochs, verbose=verbose, validation_split = validation_split, batch_size = batch_size)
 
 	def save(self, version):
-		self.model.save(run_folder + 'models/version' + "{0:0>4}".format(version) + '.h5')
+		self.model.save('./model_' + '{0:0>4}'.format(version) + '.h5')
 
-	def load(self, game, run_number, version):
-		return load_model( run_archive_folder + game + '/run' + str(run_number).zfill(4) + "/models/version" + "{0:0>4}".format(version) + '.h5', custom_objects={'softmax_cross_entropy_with_logits': softmax_cross_entropy_with_logits})
+	def load(self, version):
+		return load_model('./models_' + '{0:0>4}'.format(version) + '.h5', custom_objects={'softmax_cross_entropy_with_logits': softmax_cross_entropy_with_logits})
 
-	def _build_model(self, input_dim):
+	def _build_model(self, input_dim, output_dim, learning_rate, momentum=0.9):
 
 		main_input = Input(shape = input_dim, name = 'main_input')
 
@@ -36,48 +50,44 @@ class Residual_CNN():
 				x = self.residual_layer(x, h['filters'], h['kernel_size'])
 
 		vh = self.value_head(x)
-		ph = self.policy_head(x)
+		ph = self.policy_head(x, output_dim)
 
 		model = Model(inputs=[main_input], outputs=[vh, ph])
 		model.compile(loss={'value_head': 'mean_squared_error', 'policy_head': softmax_cross_entropy_with_logits},
-			optimizer=SGD(lr=self.learning_rate, momentum = config.MOMENTUM),	
+			optimizer=SGD(lr=learning_rate, momentum=momentum),	
 			loss_weights={'value_head': 0.5, 'policy_head': 0.5}	
-			)
+		)
 
 		return model
 
 	def residual_layer(self, input_block, filters, kernel_size):
-
 		x = self.conv_layer(input_block, filters, kernel_size)
 
 		x = Conv2D(
-		filters = filters
-		, kernel_size = kernel_size
-		, data_format="channels_first"
-		, padding = 'same'
-		, use_bias=False
-		, activation='linear'
-		, kernel_regularizer = regularizers.l2(self.reg_const)
+			filters = filters,
+			kernel_size = kernel_size,
+			data_format='channels_first',
+			padding = 'same',
+			use_bias=False,
+			activation='linear',
+			kernel_regularizer = regularizers.l2(self.reg_const)
 		)(x)
 
 		x = BatchNormalization(axis=1)(x)
-
 		x = add([input_block, x])
-
 		x = LeakyReLU()(x)
 
 		return (x)
 
 	def conv_layer(self, x, filters, kernel_size):
-
 		x = Conv2D(
-		filters = filters
-		, kernel_size = kernel_size
-		, data_format="channels_first"
-		, padding = 'same'
-		, use_bias=False
-		, activation='linear'
-		, kernel_regularizer = regularizers.l2(self.reg_const)
+			filters = filters,
+			kernel_size = kernel_size,
+			data_format='channels_first',
+			padding = 'same',
+			use_bias=False,
+			activation='linear',
+			kernel_regularizer = regularizers.l2(self.reg_const)
 		)(x)
 
 		x = BatchNormalization(axis=1)(x)
@@ -86,54 +96,49 @@ class Residual_CNN():
 		return (x)
 
 	def value_head(self, x):
-
 		x = Conv2D(
-		filters = 1
-		, kernel_size = (1,1)
-		, data_format="channels_first"
-		, padding = 'same'
-		, use_bias=False
-		, activation='linear'
-		, kernel_regularizer = regularizers.l2(self.reg_const)
+			filters = 1,
+			kernel_size = (1,1),
+			data_format='channels_first',
+			padding = 'same',
+			use_bias=False,
+			activation='linear',
+			kernel_regularizer = regularizers.l2(self.reg_const)
 		)(x)
 
 
 		x = BatchNormalization(axis=1)(x)
 		x = LeakyReLU()(x)
-
 		x = Flatten()(x)
 
 		x = Dense(
-			20
-			, use_bias=False
-			, activation='linear'
-			, kernel_regularizer=regularizers.l2(self.reg_const)
-			)(x)
+			20,
+			use_bias=False,
+			activation='linear',
+			kernel_regularizer=regularizers.l2(self.reg_const)
+		)(x)
 
 		x = LeakyReLU()(x)
-
 		x = Dense(
-			1
-			, use_bias=False
-			, activation='tanh'
-			, kernel_regularizer=regularizers.l2(self.reg_const)
-			, name = 'value_head'
-			)(x)
-
-
+			1,
+			use_bias=False,
+			activation='tanh',
+			kernel_regularizer=regularizers.l2(self.reg_const),
+			name = 'value_head'
+		)(x)
 
 		return (x)
 
-	def policy_head(self, x):
+	def policy_head(self, x, output_dim):
 
 		x = Conv2D(
-		filters = 2
-		, kernel_size = (1,1)
-		, data_format="channels_first"
-		, padding = 'same'
-		, use_bias=False
-		, activation='linear'
-		, kernel_regularizer = regularizers.l2(self.reg_const)
+			filters = 2,
+			kernel_size = (1,1),
+			data_format='channels_first',
+			padding = 'same',
+			use_bias=False,
+			activation='linear',
+			kernel_regularizer = regularizers.l2(self.reg_const)
 		)(x)
 
 		x = BatchNormalization(axis=1)(x)
@@ -142,7 +147,7 @@ class Residual_CNN():
 		x = Flatten()(x)
 
 		x = Dense(
-			self.output_dim,
+			output_dim,
 			use_bias=False,
 			activation='linear',
 			kernel_regularizer=regularizers.l2(self.reg_const),
