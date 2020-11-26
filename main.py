24e1ff198f9ad793ce089264ffda2a5cebaae0a1
bastiaanv <bastiankpn7800@gmail.com>
2020-11-26 13:05:00 +0100

    Quick push

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+from model import Residual_CNN
+
+import gym
+
 import wandb
 wandb.init(project="Data Challenge 2 - Go")
 
+# Hyperparameter
+REG_CONST = 0.0001
+LEARNING_RATE = 0.1
+BOARD_SIZE = 9
+KOMI = 0
+
+HIDDEN_CNN_LAYERS = [
+	{'filters':75, 'kernel_size': (4,4)},
+    {'filters':75, 'kernel_size': (4,4)},
+    {'filters':75, 'kernel_size': (4,4)},
+    {'filters':75, 'kernel_size': (4,4)},
+    {'filters':75, 'kernel_size': (4,4)},
+    {'filters':75, 'kernel_size': (4,4)}
+]
+
+
+# Init environment
+env = gym.make('gym_go:go-v0', size=BOARD_SIZE, komi=KOMI, reward_method='real')
+
+# Init models
+current_NN = Residual_CNN(REG_CONST, LEARNING_RATE, env.observation_space.shape, env.action_space.shape[0], HIDDEN_CNN_LAYERS)
