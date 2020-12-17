fa7b5d96d7026864a9b8940c30ecd194ee45189c
bastiaanv <bastiankpn7800@gmail.com>
2020-12-17 13:17:06 +0100

    Hotfix comment

--- gobang/NeuralNet.py
+++ gobang/NeuralNet.py
@@ -18,7 +18,7 @@ class GobangNNet():
         h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='valid')(h_conv2)))        # batch_size  x (board_x-4) x (board_y-4) x num_channels
         h_conv3_flat = Flatten()(h_conv3)       
         s_fc1 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024)(h_conv3_flat))))                # batch_size x 1024
-        s_fc2 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512)(s_fc1))))                        # batch_size x 1024
+        s_fc2 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512)(s_fc1))))                        # batch_size x 512
         self.pi = Dense(self.action_size, activation='softmax', name='pi')(s_fc2)                                               # batch_size x self.action_size
         self.v = Dense(1, activation='tanh', name='v')(s_fc2)                                                                   # batch_size x 1
 
